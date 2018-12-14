06a6799c7b53228b9756094d03d1bd043ab05408
Elizabeth J <liz.jenerson@gmail.com>
2018-12-14 08:04:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const currentUser = "Grace Hopper";
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
